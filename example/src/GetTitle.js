92fc43d68478d200e4268f9bf54dded7fb3c95af
emb2ug <47042768+emb2ug@users.noreply.github.com>
2019-05-27 11:42:19 -0400

    functionality of library search working

--- example/src/GetTitle.js
+++ example/src/GetTitle.js
@@ -1,20 +1,48 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import axios from "axios";
 
 export default class GetTitle extends React.Component {
   constructor(props) {
     super(props);
 
-    this.state = {};
+    this.state = { bookAuthor: "" };
   }
 
-  getAuthor = title => {
-    axios.get("localhost:3000/book?title=" + title);
+  componentDidMount = () => {};
+
+  getAuthor = () => {
+    let title = document.getElementById("inputTitle").value;
+    console.log(title);
+    let url = "http://localhost:9000/books/" + title;
+    console.log(url);
+    axios
+      .get(url)
+
+      .then(res => {
+        console.log(res.data);
+        this.setState({ bookAuthor: res.data });
+      });
   };
 
   render() {
-    return <div />;
+    return (
+      <div className="App">
+        <header className="App-header">
+          <br />
+          <input type="text" id="inputTitle" placeholder="Enter Title" />
+          <br />
+          <button onClick={() => this.getAuthor()}>Search</button>
+          <br />
+          {this.state.bookAuthor.length > 0 ? (
+            <p>{this.state.bookAuthor}</p>
+          ) : (
+            <div />
+          )}
+        </header>
+      </div>
+    );
   }
 }
 
--- server.js
+++ server.js
@@ -1,18 +1,44 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
-const port = 3000;
+app.use(cors());
+const port = 9000;
 
 books = {
-  "1984": "George Orwell"
+  "1984": "George Orwell",
+  HarryPotter: "JK Rowling"
 };
 
-//app.get("/", (req, res) => res.send("Hello World!"));
+// app.get("/", (req, res) => res.send("Hellooooooooooo World!"));
 
-app.get("/book:title"),
-  (req, res) => {
-    const ans = books[req.params.title];
+// app.get("/test", (req, res) => res.send("got here"));
+
+// app.get("/books", (req, res) => res.send(books));
+
+// app.get("/books/:title"),
+//   (req, res) => {
+//     console.log(req.params.title);
+//     const ans = books["" + req.params.title];
+//     res.send(ans);
+//   };
+
+app.get("/:title", (req, res) => {
+  const ans = books[req.params.title];
+  if (ans) {
     res.send(ans);
-  };
+  } else {
+    res.send("Cannot find author for " + req.params.title);
+  }
+});
+
+app.get("/books/:title", (req, res) => {
+  const ans = books[req.params.title];
+  if (ans) {
+    res.send(ans);
+  } else {
+    res.send("Cannot find author for " + req.params.title);
+  }
+});
 
 // app.get('/getGeocode:address', (req, res) => {
 //     axios.get(google api url).then(
